cf20e31a11bca13e1c282e031fdd2308c0559b86
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2022-03-21 21:00:11 +0900

    [UPDATE] 220321

--- src/pages/With-Redux-App.js
+++ src/pages/With-Redux-App.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
 import './With-Redux-App.css';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
+function reducer(currentState, action) {
+  // 기존 state 값이 undefined 인 경우, 즉 없는 경우 초기값을 설정해주는 조건문이다.
+  if (currentState === undefined) {
+    return {
+      number: 1,
+    }
+  }
+  // const newState = Object.assign({}, currentState); 와 동일하다.
+  // 기존 state 값의 복사본을 만드는 것이다.
+  const newState = { ...currentState };
+  if (action.type === 'PLUS') {
+    newState.number++;
+  }
+  return newState;
+}
+
+const store = createStore(reducer);
 
 function WithReduxApp() {
-  const [number, setNumber] = useState(1);
-
   return (
     <div id="container">
-      <h1>Root: {number}</h1>
+      <h1>Root: </h1>
       <div id="grid">
-        <Left1 number={number} />
-        <Right1 onIncrease={() => { setNumber(number + 1) }} />
+        {/* state 를 어떤 컴포넌트에 제공할 것인지 범위를 결정하는 울타리가 Provider 이다. */}
+        {/* Provider 는 반드시 props 를 설정해줘야 한다. */}
+        <Provider store={store}>
+          <Left1 />
+          <Right1 />
+        </Provider>
       </div>
     </div>
   );
@@ -18,8 +40,8 @@ function WithReduxApp() {
 function Left1(props) {
   return (
     <div>
-      <h1>Left1: {props.number}</h1>
-      <Left2 number={props.number} />
+      <h1>Left1: </h1>
+      <Left2 />
     </div>
   )
 }
@@ -27,16 +49,19 @@ function Left1(props) {
 function Left2(props) {
   return (
     <div>
-      <h1>Left2: {props.number}</h1>
-      <Left3 number={props.number} />
+      <h1>Left2: </h1>
+      <Left3 />
     </div>
   )
 }
 
 function Left3(props) {
+  // useSelector 는 state 값을 호출하는 함수로, 반드시 함수를 인자로 갖는다.
+  // 예를 들어, 아래와 같이 작성한 경우 state 중에서 number 라는 속성에 해당하는 값을 호출할 수 있다.
+  const number = useSelector(state => state.number);
   return (
     <div>
-      <h1>Left3: {props.number}</h1>
+      <h1>Left3: {number}</h1>
     </div>
   )
 }
@@ -45,7 +70,7 @@ function Right1(props) {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2 onIncrease={() => props.onIncrease()} />
+      <Right2 />
     </div>
   )
 }
@@ -54,16 +79,19 @@ function Right2(props) {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3 onIncrease={() => props.onIncrease()} />
+      <Right3 />
     </div>
   )
 }
 
 function Right3(props) {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
-      <input type="button" value="+" onClick={() => { props.onIncrease() }}></input>
+      <input type="button" value="+" onClick={() => {
+        dispatch({ type: 'PLUS' })
+      }}></input>
     </div>
   )
 }
